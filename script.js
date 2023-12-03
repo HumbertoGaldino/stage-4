e818a5ca75c70bf973ca45e1a30bad7c2470b6b5
Humberto Galdino <humberto.galdino@live.com>
2023-12-03 18:14:34 -0300

    Aula 2

--- script.js
+++ script.js
@@ -4,6 +4,17 @@
   Pergunte o nome do usuário e escreva a mensagem:
   "Olá, [nome do usuário]"
 
-*/
+
 let nome = prompt("Qual o seu nome?")
-alert("Olá, " + nome)
+alert("Olá, " + nome) */
+
+/* 
+  Solicite 2 números, faça a soma deles
+  e apresente o resultado final ao usuário
+*/
+
+let numberOne = Number(prompt("Qual o primeiro número?"));
+let numberTwo = Number(prompt("Qual o segundo número?"));
+let soma = numberOne + numberTwo;
+
+alert("A soma dos números é " + soma);
